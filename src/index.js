62569d77fa98024a43948ac528b87e34c061d742
Zdenek Rejda <zdenek.rejda@gmail.com>
2016-11-09 14:19:25 +0100

    Add redux store, reducer and basic tasks for adding, removing and sorting data inside store.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,81 @@
-var x = ["Viléme"];
+import {
+    createStore
+} from 'redux';
 
-var y = [ //Spread Array direktiva sečte dvě pole
-    ...x,
-    "Ahoj"
-];
+/* Initial state of reducer - 2 initial tasks */
+var initialState = ['First task', 'Second task', 'Active task'];
 
-document.write(`${y[1]} ${y[0]}`);
+/* REDUCER */
+function todo(state = initialState, action) {
+
+    switch (action.type) {
+        /* Firts action - add new  task*/
+        case 'ADD':
+            return [...state, action.text];
+
+            /* Second action - remove task */
+        case 'REMOVE':
+            var newState = [...state];
+            newState.splice(action.id, 1);
+            return newState;
+
+            /* Third action - sort tasks */
+        case 'SORT':
+            if(action.sort_up) {
+                state.sort((a, b) => (a < b));
+            }
+            else {
+                state.sort((a, b) => (a > b));
+            }
+            return state;
+
+            /* Fourth action - remove all tasks */
+        case 'REMOVE-ALL':
+            return [];
+
+        default:
+            return state;
+    }
+}
+
+/* Create store */
+var store = createStore(todo);
+
+/* Component for writing store content to console and to page */
+store.subscribe(function() {
+    var state = store.getState();
+    console.log('New state:', state);
+    document.body.innerHTML = 'Actual state:<br/>- ' + state.join('<br/>- ');
+});
+
+/* few actions for test */
+store.dispatch({
+    type: 'ADD',
+    text: 'Third task'
+});
+
+store.dispatch({
+    type: 'ADD',
+    text: 'Fourth task'
+});
+
+store.dispatch({
+    type: 'REMOVE',
+    id: 1
+});
+
+store.dispatch({
+    type: 'SORT',
+    sort_up: true
+});
+
+store.dispatch({
+    type: 'SORT',
+    sort_up: false
+});
+
+setTimeout(() => {
+    store.dispatch({
+        type: 'REMOVE-ALL'
+    });
+}, 10000);
