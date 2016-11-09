e0ec91c0c24e87a3bbeefea3acf45929adf0e28d
Zdenek Rejda <zdenek.rejda@gmail.com>
2016-11-09 10:28:56 +0100

    Základní nastavení webpacku

---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    entry: "./src/index.js",  //náš vstupní bod aplikace
+    output: {
+        filename: "bundle.js"   //výstupní balík všech zdrojových kódů
+    },
+    module: { //sem budeme zanedlouho vkládat transformační moduly
+    }
+};
