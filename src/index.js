805906ef47eea8ed0db95db5837515d38e229622
Zdenek Rejda <zdenek.rejda@gmail.com>
2016-11-09 12:56:18 +0100

    Přidána podpora ES6 pomocí Babelu.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-document.write("Hello world");
+var x = ["Viléme"];
+
+var y = [ //Spread Array direktiva sečte dvě pole
+    ...x,
+    "Ahoj"
+];
+
+document.write(`${y[1]} ${y[0]}`);
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,16 @@
 module.exports = {
-    entry: "./src/index.js",  //náš vstupní bod aplikace
+    entry: "./src/index.js", //náš vstupní bod aplikace
     output: {
-        filename: "bundle.js"   //výstupní balík všech zdrojových kódů
+        filename: "bundle.js" //výstupní balík všech zdrojových kódů
     },
-    module: { //sem budeme zanedlouho vkládat transformační moduly
+    module: {
+        loaders: [{
+            test: /\/src\/.+\.js$/, //Všechny soubory s koncovkou js...
+            loader: 'babel-loader', //prožeň babel-loaderem (integrace babelu a webpacku)
+            query: {
+                presets: ['react', 'es2015'], //vybrané babel presety: https://babeljs.io/docs/plugins/#presets
+                plugins: ["transform-class-properties"] //vybrané pluginy https://babeljs.io/docs/plugins/#transform-plugins
+            }
+        }]
     }
 };
